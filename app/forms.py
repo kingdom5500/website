4d859bd7695124af1fe4bd834392074a3a64795a
kingdom5500 <kingsleymcdonald@mail.com>
2018-06-08 22:24:02 +0100

    Oh, tabs.

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import Length, DataRequired
 
 
 class PostForm(FlaskForm):
-	"""The form used to send messages on the 'msg.' subdomain."""
+    """The form used to send messages on the 'msg.' subdomain."""
     name = TextField('name', validators=[Length(max=32)])
     message = TextAreaField('message', validators=[Length(max=2048), DataRequired()])
